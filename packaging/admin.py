a6bbea6a5eb132a86226e44f0243a638ec00bb1b
Syed Arhan <arhannaqshbandi@gmail.com>
2026-09-01 22:33:19 +0530

    Register packing models in Django admin

--- packaging/admin.py
+++ packaging/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Box, Order, OrderItem, Product
+
+admin.site.register(Box)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
